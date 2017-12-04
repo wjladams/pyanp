ada460c8d5cd5d42c234c304bb165babe7c9727a
William Adams <wjadams@decisionlens.com>
2017-12-04 10:49:30 -0500

    Forgot to add setup files somehow.

---
+++ setup.py
@@ -0,0 +1,21 @@
+'''
+Created October 24, 2017
+
+@author: Bill Adams
+'''
+
+from setuptools import setup
+
+setup(name='pyanp',
+      version='0.1.0',
+      description='Math library for ANP/AHP calculations',
+      url='Not yet given',
+      author='Bill Adams',
+      author_email='wjadams@decisionlens.com',
+      license='MIT',
+      package_dir={'pyanp':'pyanp'},
+      python_requires=">=3",
+      packages=['pyanp'],
+      #py_modules=['fxmodel'],
+      #package_data={'bamath': ['data/*.csv', 'data/*.pickle', 'data/*.json', '*.css']},
+      zip_safe=False)
